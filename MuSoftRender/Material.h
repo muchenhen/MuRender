54413853e00eebc03114fff5e9b7e11b7e9a1d29
muchenhen <44308876+muchenhen@users.noreply.github.com>
2024-07-17 22:13:29 +0800

    添加材质类型

--- MuSoftRender/Material.h
+++ MuSoftRender/Material.h
@@ -1,1 +1,51 @@
 #pragma once
+#include <vector>
+#include <Eigen/Core>
+
+class Material
+{
+public:
+    struct TextureData
+    {
+        std::vector<unsigned char> data;
+        int width;
+        int height;
+    };
+
+    Material() :
+        baseColor(1, 1, 1)
+    {
+    }
+
+    void SetBaseColor(const Eigen::Vector3f& color)
+    {
+        baseColor = color;
+    }
+
+    void SetTexture(const TextureData& tex)
+    {
+        texture = tex;
+    }
+
+    Eigen::Vector3f GetColor(const Eigen::Vector2f& texCoord) const
+    {
+        if (texture.data.empty())
+        {
+            return baseColor;
+        }
+
+        int x = static_cast<int>(texCoord.x() * texture.width) % texture.width;
+        int y = static_cast<int>(texCoord.y() * texture.height) % texture.height;
+        int index = (y * texture.width + x) * 3;
+
+        return Eigen::Vector3f(
+                   texture.data[index] / 255.0f,
+                   texture.data[index + 1] / 255.0f,
+                   texture.data[index + 2] / 255.0f)
+               * baseColor;
+    }
+
+private:
+    Eigen::Vector3f baseColor;
+    TextureData texture;
+};
