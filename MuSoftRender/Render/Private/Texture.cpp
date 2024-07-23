8f2b72d2a1af27b6834bd403ad7e8f5ca80d053f
muchenhen <44308876+muchenhen@users.noreply.github.com>
2024-07-23 23:03:12 +0800

    简单优化了一下Texture Sample函数

--- MuSoftRender/Render/Private/Texture.cpp
+++ MuSoftRender/Render/Private/Texture.cpp
@@ -7,7 +7,10 @@ bool Texture::LoadFromFile(const std::string& Filename)
 {
     int Channels;
     unsigned char* Image = stbi_load(Filename.c_str(), &Width, &Height, &Channels, 3);
+
     if (!Image) return false;
+
+    CheckIsPowerOf2();
 
     std::ptrdiff_t DataSize = static_cast<std::ptrdiff_t>(Width) *
                               static_cast<std::ptrdiff_t>(Height) *
@@ -21,14 +24,24 @@ Eigen::Vector3f Texture::Sample(const Eigen::Vector2f& TexCoord) const
 {
     if (Data.empty()) return Eigen::Vector3f::Ones();
 
-    float u = std::fmod(TexCoord.x(), 1.0f);
-    float v = std::fmod(TexCoord.y(), 1.0f);
-    if (u < 0) u += 1.0f;
-    if (v < 0) v += 1.0f;
+    uint32_t u, v;
 
-    int X = static_cast<int>(u * (Width - 1));
-    int Y = static_cast<int>(v * (Height - 1));
-    int Index = (Y * Width + X) * 3;
+    if (IsPowerOfTwo)
+    {
+        u = static_cast<uint32_t>(TexCoord.x() * WidthMinus1) & WidthMask;
+        v = static_cast<uint32_t>(TexCoord.y() * HeightMinus1) & HeightMask;
+    }
+    else
+    {
+        float uf = fmod(TexCoord.x(), 1.0f);
+        float vf = fmod(TexCoord.y(), 1.0f);
+        if (uf < 0.0f) uf += 1.0f;
+        if (vf < 0.0f) vf += 1.0f;
+        u = static_cast<uint32_t>(uf * WidthMinus1);
+        v = static_cast<uint32_t>(vf * HeightMinus1);
+    }
+
+    uint32_t Index = (v * Width + u) * 3;
     
     return {
         static_cast<float>(Data[Index]) / 255.0f,
--- MuSoftRender/Render/Public/Texture.h
+++ MuSoftRender/Render/Public/Texture.h
@@ -5,17 +5,27 @@
 #pragma warning(default : 4819)
 #include <string>
 
-
 class Texture
 {
 private:
     std::vector<unsigned char> Data;
     int Width;
     int Height;
+    int WidthMinus1;
+    int HeightMinus1;
+    uint32_t WidthMask;
+    uint32_t HeightMask;
+    static constexpr float Inv255 = 1.0f / 255.0f;
+    bool IsPowerOfTwo;
+
+    static bool IsPowerOf2(int Value)
+    {
+        return (Value & (Value - 1)) == 0 && Value > 0;
+    }
 
 public:
     Texture() :
-        Width(0), Height(0)
+        Width(0), Height(0), WidthMinus1(0), HeightMinus1(0)
     {
     }
 
@@ -23,7 +33,38 @@ public:
     {
         Width = InWidth;
         Height = InHeight;
+
+        CheckIsPowerOf2();
+
         Data.resize(static_cast<size_t>(Width) * Height * 3);
+    }
+
+    void CheckIsPowerOf2()
+    {
+        if (Width > 1)
+        {
+            WidthMinus1 = Width - 1;
+        }
+        else
+        {
+            WidthMinus1 = 0;
+        }
+        if (Height > 1)
+        {
+            HeightMinus1 = Height - 1;
+        }
+        else
+        {
+            HeightMinus1 = 0;
+        }
+
+        IsPowerOfTwo = IsPowerOf2(Width) && IsPowerOf2(Height);
+
+        if (IsPowerOfTwo)
+        {
+            WidthMask = WidthMinus1;
+            HeightMask = HeightMinus1;
+        }
     }
 
     bool LoadFromFile(const std::string& Filename);
