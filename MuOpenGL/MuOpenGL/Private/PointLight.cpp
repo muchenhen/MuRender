e23c9d3fb2f0ff8d769bfe3a8fadce8105dcebcf
muchenhen <44308876+muchenhen@users.noreply.github.com>
2024-08-26 21:15:18 +0800

    添加点光源

---
+++ MuOpenGL/MuOpenGL/Private/PointLight.cpp
@@ -0,0 +1,35 @@
+﻿#include "PointLight.h"
+
+PointLight::PointLight(const std::string& name):
+    m_color(1.0f, 1.0f, 1.0f),
+    m_intensity(1.0f),
+    m_constant(0),
+    m_linear(0),
+    m_quadratic(0)
+{
+}
+
+glm::vec3 PointLight::GetColor() const
+{
+    return m_color;
+}
+
+float PointLight::GetIntensity() const
+{
+    return m_intensity;
+}
+
+float PointLight::GetConstant() const
+{
+    return m_constant;
+}
+
+float PointLight::GetLinear() const
+{
+    return m_linear;
+}
+
+float PointLight::GetQuadratic() const
+{
+    return m_quadratic;
+}
---
+++ MuOpenGL/MuOpenGL/Public/PointLight.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "Object.h"
+
+class PointLight : public Object
+{
+public:
+    PointLight(const std::string& name = "PointLight");
+
+    void SetColor(const glm::vec3& color) { m_color = color; }
+    void SetIntensity(float intensity) { m_intensity = intensity; }
+
+    void SetAttenuation(float constant, float linear, float quadratic)
+    {
+        m_constant = constant;
+        m_linear = linear;
+        m_quadratic = quadratic;
+    }
+
+    glm::vec3 GetColor() const;
+    float GetIntensity() const;
+    float GetConstant() const;
+    float GetLinear() const;
+    float GetQuadratic() const;
+
+private:
+    glm::vec3 m_color;
+    float m_intensity;
+    float m_constant;  // 常数项
+    float m_linear;    // 一次项
+    float m_quadratic; // 二次项
+};
